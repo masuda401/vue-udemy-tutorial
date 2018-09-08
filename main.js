58cb0258adc3de6b5e45becad1045204414f8a00
Shinichi Masuda <shinichi.masuda@gmail.com>
2018-09-08 13:58:59 +0900

    実践演習：Bitconin価格表示アプリの開発 - API連携 データを格納するプロパティを作成

--- main.js
+++ main.js
@@ -1,23 +1,6 @@
 var app = new Vue({
     el: '#app',
     data: {
-        newItem: '',
-        todos: []
-    },
-    methods: {
-        addItem: function (event) {
-            if(this.newItem == '') return
-
-            var todo = {
-                item: this.newItem,
-                isDone: false
-            }
-
-            this.todos.push(todo)
-            this.newItem = ''
-        },
-        deleteItem: function (index) {
-            this.todos.splice(index, 1)
-        }
+        bpi: null
     }
 })
