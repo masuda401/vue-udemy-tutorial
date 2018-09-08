39a7684ca7192ac693c7adfe3c6a0866bc95f591
Shinichi Masuda <shinichi.masuda@gmail.com>
2018-09-08 20:56:18 +0900

    小数点第二位まで表示するように修正

--- main.js
+++ main.js
@@ -11,5 +11,10 @@ var app = new Vue({
             .catch(function (error) {
                 console.log(error)
             })
+    },
+    filters: {
+        currencyDecimal(value) {
+            return value.toFixed(2)
+        }
     }
 })
